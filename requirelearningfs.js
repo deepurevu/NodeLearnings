56b963a41f9e416ca6c627f3b92621327f7499e4
Kumar <sharan.kumar@cardinalhealth.com>
2017-07-03 23:56:03 -0500

    fsos

--- requirelearningfs.js
+++ requirelearningfs.js
@@ -1,7 +1,10 @@
- console.log('Starting the app..')
+ console.log('Starting the app..');
  const fs=require('fs');
- fs.appendFile('greeting.txt','HelloWorld'); //Will show a warning
- fs.appendFile('sharan.txt','withcallbackerro',function(err){
+ const os=require('os');
+ var user=os.userInfo();
+
+ fs.appendFile('greeting.txt','HelloWorld'+user.username); //Will show a warning
+ fs.appendFile('sharan.txt',`Using ES6 Templat print ${user.username}${user.userInfo}`,function(err){
     if(err){
      console.log("something bad happend");
     }
