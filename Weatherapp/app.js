f1240722282e3a88665c0255c8777aed7ce08d10
Kumar <sharan.kumar@cardinalhealth.com>
2017-07-22 23:11:26 -0500

    updated:

--- Weatherapp/app.js
+++ Weatherapp/app.js
@@ -24,9 +24,17 @@ request({
   url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
   json: true    //dont put capital JSON it will throw no results, happened in training
 }, (error, response, body) => {
-  console.log(`Address: ${body.results[0].formatted_address}`);
-  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-  console.log(`Longitude: ${body.results[0].geometry.location.lng}`)
+     if (error){
+        console.log("Not able to establish connection with servers");
+    } else if (body.status==='ZERO_RESULTS'){
+         console.log("Invalid address");
+    } else if (body.status==='OK'){
+
+         console.log(`Address: ${body.results[0].formatted_address}`);
+         console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+         console.log(`Longitude: ${body.results[0].geometry.location.lng}`)
+    }
+
 });
 
 
--- Weatherapp/otherapp.js
+++ Weatherapp/otherapp.js
@@ -1,6 +1,10 @@
+
+//Program will accept the user input address using Yargs module and set the options for passing address by -a 'node app.js -a '1360 south white' and it has to be encoded
+//and then call google maps API uinhg http request from request package and display the latitude and longitude
+
 const request = require('request');
 const yargs = require('yargs');
-
+//Create a new directory caled geocode and import geocode function here
 const argv = yargs
   .options({
     a: {
@@ -14,13 +18,9 @@ const argv = yargs
   .alias('help', 'h')
   .argv;
 
-var encodedAddress = encodeURIComponent(argv.address);
+geocode.geocodeaddresss(argv.address)//create this function in geocode.js
 
-request({
-  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
-  json: true
-}, (error, response, body) => {
-  console.log(`Address: ${body.results[0].formatted_address}`);
-  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-  console.log(`Longitude: ${body.results[0].geometry.location.lng}`)
-});
+
+
+
+    //request(url,callbackfunction(){});//request takes two parameters and once url result come back it execites the callback function and that time only call back function gets executed.
